b5df44b43fd2684a0c2f4215d7f647b52746b71d
Alexander van Dam (fontys) <a.vandam@fontys.nl>
2018-03-07 19:51:49 +0100

    Fixed database connection

--- Source/RaceStatistics.Dal/TestContext.cs
+++ Source/RaceStatistics.Dal/TestContext.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,32 @@ namespace RaceStatistics.Dal
 {
     public class TestContext
     {
+
+        public string GetTest()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectionSettings.GetConnectionString()))
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM test;";
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    {
+                        //cmd.Parameters.AddWithValue("@ID", id);
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                return Convert.ToString(reader["Test"]);
+                            }
+                            return null;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { throw ex; }
+        }
+
+
     }
 }
---
+++ Source/RaceStatistics.DalTests/TestContextTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RaceStatistics.Dal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceStatistics.Dal.Tests
+{
+    [TestClass]
+    public class TestContextTests
+    {
+        [TestMethod]
+        public void GetTestTest()
+        {
+            TestContext testContext = new TestContext();
+            Assert.AreEqual("abcd", testContext.GetTest());
+        }
+    }
+}
