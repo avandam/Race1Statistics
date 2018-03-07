561c7c741210b5dcf6b19a4d2652223e8a6eaf6e
Alexander van Dam (fontys) <a.vandam@fontys.nl>
2018-03-07 15:10:58 +0100

    Added project with basic connection settings

---
+++ Source/RaceStatistics.Dal/ConnetionSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceStatistics.Dal
+{
+    public static class ConnetionSettings
+    {
+        public static string SshServerName = "";
+        public static string SshUsername = "";
+        public static string sshPassword = "";
+
+        public static string DatabaseServer = "";
+        public static string DatabaseName = "";
+        public static string DatabaseUser = "";
+        public static string DatabasePassword = "";
+        public static bool DatabaseAllowBatch = true;
+    }
+}
