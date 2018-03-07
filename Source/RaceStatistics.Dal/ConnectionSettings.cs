bb67123ee9690f7c2f38060c77d30c811b2b3d40
Alexander van Dam (fontys) <a.vandam@fontys.nl>
2018-03-07 19:51:49 +0100

    Fixed database connection

---
+++ Source/RaceStatistics.Dal/ConnectionSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceStatistics.Dal
+{
+    public static class ConnectionSettings
+    {
+        public static string DatabaseServer = "";
+        public static string DatabaseName = "";
+        public static string DatabaseUser = "";
+        public static string DatabasePassword = "";
+
+        public static string GetConnectionString()
+        {
+            SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+            stringBuilder.UserID = DatabaseUser;
+            stringBuilder.Password = DatabasePassword;
+            stringBuilder.DataSource = DatabaseServer;
+            stringBuilder.InitialCatalog = DatabaseName;
+
+            return stringBuilder.ConnectionString;
+        }
+    }
+}
